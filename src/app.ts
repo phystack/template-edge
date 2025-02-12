22c442630f04eec40cbd11f338ce4dd9546fe079
Andreas Hassellof <andreas@ombori.com>
2025-02-12 16:51:13 +0400

    fix: new sample code and updated dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-import { Settings } from './schema.js';
+import { PhyHubClient, connectPhyClient } from '@phygrid/hub-client';
+import { Settings } from './schema';
+
+const client: PhyHubClient = await connectPhyClient();
+const settings = await client.getEdgeSettings() as Settings;
+
+let counter = 0;
+setInterval(() => {
+  counter += + 1;
+  console.log('Hello, world!', counter);
+  console.log('Settings:', JSON.stringify(settings, null, 2));
+}, 3000);
