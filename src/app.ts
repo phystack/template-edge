8999169c9cb9789af8a98e797c8029a2c0bb4b41
Andreas Hassellof <andreas@ombori.com>
2025-02-18 17:22:00 +0400

    fix: new settings format

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import { PhyHubClient, connectPhyClient } from '@phygrid/hub-client';
 import { Settings } from './schema';
 
 const client: PhyHubClient = await connectPhyClient();
-const settings = await client.getEdgeSettings() as Settings;
+const instance = await client.getInstance();
+const settings = await client.getSettings() as Settings;
 
 let counter = 0;
 setInterval(() => {
@@ -10,3 +11,7 @@ setInterval(() => {
   console.log('Hello, world!', counter);
   console.log('Settings:', JSON.stringify(settings, null, 2));
 }, 3000);
+
+instance.on('message', (message) => {
+  console.log('Message:', message);
+});
